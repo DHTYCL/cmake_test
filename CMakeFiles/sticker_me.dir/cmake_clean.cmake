24a3e04bb85f67e043d204a0d95a91c8f71c7af7
DHTYCL <594375373@qq.com>
2018-08-27 21:10:18 +0800

    first commit

---
+++ CMakeFiles/sticker_me.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sticker_me.dir/main.cpp.o"
+  "sticker_me.pdb"
+  "sticker_me"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sticker_me.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include"fun/fun.h"
+using namespace std;
+int main()
+{
+std::cout<<"main"<<std::endl;
+funs();
+return 0;
+}
